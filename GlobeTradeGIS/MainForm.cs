bd3bcf8e6477bc032b08c3209c547cabb0bf5117
tang-xy <a123654789654@yeah.net>
2018-09-06 15:50:46 +0800

    添加项目文件。

---
+++ GlobeTradeGIS/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.XtraEditors;
+
+namespace GlobeTradeGIS
+{
+    public partial class MainForm : XtraForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
